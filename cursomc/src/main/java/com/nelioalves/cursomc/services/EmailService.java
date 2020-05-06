9ca06cac8b3f2a95f6ae4897de49e5862b50f898
Leonardo Sena <leeosena21@gmail.com>
2020-05-06 10:06:48 -0300

    Envio de email HTML

--- cursomc/src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,14 +1,18 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
 
-	void sendOrderConfirmationEmail(Pedido obj);
-	
-	void sendEmail(SimpleMailMessage msg);
+	void sendOrderConfirmationEmail(Pedido obj); //Texto simples
+
+	void sendEmail(SimpleMailMessage msg); //Texto simples
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
-
-
